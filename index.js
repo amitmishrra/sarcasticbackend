fb7bc472b546139e583c99d89c5084168b65193a
Amit Mishra <77039681+amitmishrra@users.noreply.github.com>
2022-11-22 18:00:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ const cors = require("cors");
 app.use(express.json())
 app.use(cors())
 
-app.UseCors(builder => builder
-   .AllowAnyOrigin()
-   .AllowAnyMethod()
-   .AllowAnyHeader()
-            )
-
 app.listen(5000,(req, res) =>{
     console.log("Server is running")
 })
